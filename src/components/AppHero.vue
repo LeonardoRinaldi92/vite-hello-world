b32bd3a1ef9f68a25d2ab9620e3d2e8e86a763ea
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-05-02 16:47:43 +0200

    COMPLETO (piu cambio background al click)

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -3,29 +3,42 @@ export default{
     name: "AppHero",
     data() {
         return{
-            luna: "'/images/moon.jpg'",
-
-
-
+            sfondo: true,
         }
-    }
+    },
+    methods: {
+        cambiaSfondo(){
+            if(this.sfondo) {
+                this.sfondo = false
+            } else {
+                this.sfondo = true
+            }
+        }
+    },
 }
 </script>
 
 <template>
     <div>
-        <div class="p-5 mb-4 rounded-3" id="jumbo">
+        <div class="p-5 mb-4 rounded-3" :class="(sfondo)? 'luna': 'sole'">
             <div class="container-fluid py-5">
                 <h1 class="text-success">Ciao sono un jumbotron</h1>
-                <p class="text-info">forse...</p>
-                <button class="btn btn-dark" type="button">Non premermi sono inutile</button>
+                <p class="text-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt quaerat vel voluptate? Obcaecati commodi vero numquam qui, ea exercitationem quam. Odit obcaecati eum dolorum quas quo eligendi asperiores, ratione quia!</p>
+                <button class="btn btn-dark" type="button" @click="cambiaSfondo">Non premermi sono inutile(forse..)</button>
             </div>
         </div>
     </div>
 </template>
 
 <style scoped>
-    #jumbo {
+    .luna {
         background-image:url(/images/moon.jpg);
+    }
+
+    .sole {
+        background-image:url(/images/sole.jpeg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        filter: brightness(2);
     }
 </style>
