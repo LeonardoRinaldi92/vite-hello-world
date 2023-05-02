bab0d0f83c00e6b8c9c37743579c4bc210bbb60b
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-05-02 16:07:08 +0200

    COMPLETO(forse) MI SONO DIMENTICATO DI PUSHARE :(

---
+++ src/components/AppHero.vue
@@ -0,0 +1,31 @@
+<script>
+export default{
+    name: "AppHero",
+    data() {
+        return{
+            luna: "'/images/moon.jpg'",
+
+
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <div class="p-5 mb-4 rounded-3" id="jumbo">
+            <div class="container-fluid py-5">
+                <h1 class="text-success">Ciao sono un jumbotron</h1>
+                <p class="text-info">forse...</p>
+                <button class="btn btn-dark" type="button">Non premermi sono inutile</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    #jumbo {
+        background-image:url(/images/moon.jpg);
+    }
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+export default{
+    name: "AppTitle",
+    data() {
+        return{
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>La mia prima app con Vite</h1>
+    </div>
+</template>
+
+<style scoped>
+ h1 {
+    color: green;
+    text-align: center;
+ }
+</style>
